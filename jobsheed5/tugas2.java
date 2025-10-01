86c946f5a683663b6a906c682441b1ec7dd04c5c
Alfatitah Alifia Putri <alfatitahalifia@gmail.com>
2025-10-02 04:52:11 +0700

    tugas2

---
+++ jobsheed5/tugas2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class tugas2 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+System.out.println(" Masukkan sebuah angka :");
+int angka = sc.nextInt();
+ 
+if (angka % 2 == 0){
+    System.out.println("Angka"+ angka + " adalah genap");
+}else{
+    System.out.println("Angka"+ angka + " adalah ganjil");
+}
+
+sc.close();
+}   
+}

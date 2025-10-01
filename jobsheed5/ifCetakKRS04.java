094f1f83b2b5493fd88968582d8b33eade670fe9
Alfatitah Alifia Putri <alfatitahalifia@gmail.com>
2025-10-01 14:26:04 +0700

    modifikasi percobaan 1

--- jobsheed5/ifCetakKRS04.java
+++ jobsheed5/ifCetakKRS04.java
@@ -8,5 +8,8 @@ public static void main(String[] args) {
     if (uktLunas) {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+
+    }else{
+        System.out.println("Registrasi diolak.Silahkan lunasi UKT terlebih dahulu");
     }
 }

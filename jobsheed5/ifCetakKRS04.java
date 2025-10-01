cf8f9064bbd7ec59893417369e19b58de598ae88
Alfatitah Alifia Putri <alfatitahalifia@gmail.com>
2025-10-02 04:42:38 +0700

    tugas 1

--- jobsheed5/ifCetakKRS04.java
+++ jobsheed5/ifCetakKRS04.java
@@ -5,11 +5,21 @@ public static void main(String[] args) {
     System.out.println("---Cetak KRS SIAKAD---");
     System.out.println("Apakah UKT sudah lunas ? (true/false):");
     boolean uktLunas = sc.nextBoolean();
-    if (uktLunas) {
+   if (uktLunas) {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
 
     }else{
         System.out.println("Registrasi diolak.Silahkan lunasi UKT terlebih dahulu");
+        sc.close();
     }
-}
+    System.out.println("--- Versi Ternari Operator ---");
+    String hasil = sc.next();
+        if(uktLunas){
+            System.out.println("Pembayaran UKT terverifikasi.\nSilahkan cetak KRS dan minta tanda tangan DPA");
+        }else{
+            System.out.println("Registrasi diolak.Silahkan lunasi UKT terlebih dahulu");
+                sc.close();
+    }
+        }
+        

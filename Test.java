85d99e042d053f19c5f7d19dca26dcf21facc796
alfatitahalifia-source <alifiaputri@gmail.com>
2025-10-09 13:38:00 +0700

    TESST

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("HALOO");
         System.out.println("TEST");
+        System.out.println("");
     }
 }

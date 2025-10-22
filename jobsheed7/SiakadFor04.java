4e90fa40b555687d99f8876993a34df4c1d8d570
alfatitahalifia-source <alifiaputri@gmail.com>
2025-10-22 12:17:24 +0700

    Jawaban pertanyaan percobaan 1

--- jobsheed7/SiakadFor04.java
+++ jobsheed7/SiakadFor04.java
@@ -7,6 +7,7 @@ public class SiakadFor04 {
         Scanner sc = new Scanner(System.in);//deklarasi scanner//
 
         double nilai, tertinggi = 0, terendah = 100; //deklarasi variabel//
+        int jumlahmahasiswalulus=0, jumlahmahasiswatidaklulus=0;
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke- " + i +" : ");
             nilai = sc.nextDouble();
@@ -16,8 +17,22 @@ public class SiakadFor04 {
             if (nilai<terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                jumlahmahasiswalulus++;
+            }else {
+                jumlahmahasiswatidaklulus++;
+            }
         }
         System.out.println("Nilai tertinggi : " + tertinggi);
         System.out.println("Nilai terendah : "+ terendah);
+        System.out.println("Jumlah mahasiswa lulus "+ jumlahmahasiswalulus );
+        System.out.println("Jumlah mahasiswa tidak lulus "+ jumlahmahasiswatidaklulus );
     }
 }
+
+
+//JAWABAN PERTANYAAN//
+//1. Komponen perulangan ada inilisalisasi yang ditulis dengan int i = 1,kondisi ditulis dengan i <=10 dan perubahan kondisi yaitu yang i++
+//2. jika di balik maka nilai tertinggi tidak bisa berubah
+//3. Jika nilai lebih besar dari pada tertinggi maka nilai menjadi tertinggi , jika nilai lebih kecil dari pada terendah maka nilai menjadi terendah dan berulang sampai bats yang di tentukan
+

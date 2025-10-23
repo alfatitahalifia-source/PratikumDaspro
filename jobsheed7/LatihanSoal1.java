b342435202fce93c94d6afc64811cd18456036aa
alfatitahalifia-source <alifiaputri@gmail.com>
2025-10-23 23:11:44 +0700

    Jawaban Latihan 1

---
+++ jobsheed7/LatihanSoal1.java
@@ -0,0 +1,16 @@
+package jobsheed7;
+
+import java.util.Scanner;
+
+public class LatihanSoal1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hargatiket = 50000 , i = 0;
+        int totaltiket = 0;
+        double diskon;
+        System.out.print("masukkan total penjualan tiket :");
+        double totalHarga = totaltiket*hargatiket;
+            System.out.println("masukkan jumlah tiket :");
+    }
+}
+

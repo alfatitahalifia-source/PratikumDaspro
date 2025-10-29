eb3337e0a3682bba8f4cad4493c9630ff8514dba
alfatitahalifia-source <alifiaputri@gmail.com>
2025-10-29 11:59:23 +0700

    Jawaban percobaan 3

---
+++ Jobsheet8/Triangle04.java
@@ -0,0 +1,25 @@
+package Jobsheet8;
+
+import java.util.Scanner;
+
+public class Triangle04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+        for (int i = 0; i <= N; i++) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+        System.out.println();
+        }
+    }
+}
+
+
+//JAWABAN PERCOBAAN 3
+//1.Berhasil
+//2.tidak ada 
+

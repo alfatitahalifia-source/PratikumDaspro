9b6cb0aa6e75cb7155bc2ebfecf645fd3957783f
alfatitahalifia-source <alifiaputri@gmail.com>
2025-10-29 11:27:52 +0700

    Jawaban Percobaan 1

--- Jobsheet8/Star04.java
+++ Jobsheet8/Star04.java
@@ -7,10 +7,14 @@ public class Star04 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i <= N; i--) {
             System.out.print("*");
         }
     }
 }
 
 
+//JAWABAN PERCOBAAN 1 
+//1.Maka binatng yang di hasilkan ada 6 bukan 5 karena mulainya dari 0 bukan dari 1.
+//2.Tidak bisa mengeluarkan outputnya "*" karena kondisi awal salah.
+//3.Maka perulangan akan infinite loop karena nilai I makin kecil dan tak pernah > N.

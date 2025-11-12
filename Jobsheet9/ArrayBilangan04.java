6b5b1ddb87e5257c411e50f74a62590ba664c1d9
alfatitahalifia-source <alfatitahalifia@gmail.com>
2025-11-12 21:41:35 +0700

    Jawaban Percobaan 1

--- Jobsheet9/ArrayBilangan04.java
+++ Jobsheet9/ArrayBilangan04.java
@@ -2,11 +2,25 @@ package Jobsheet9;
 
 public class ArrayBilangan04 {
     public static void main(String[] args) {
-        int [] bil=new int[4];
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+        int [] bil={5,13,-7,17};
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
+    }
+}
+
+//Jawaban Pertanyaan
+/*
+1. Jika isi masing-masing elemen array bil diubah dengan angka 5.0, 12867, 7.5, 2000000. Apa yang terjadi? Mengapa bisa demikian?
+jika menggunakan tipe data int maka akan eror,sehingga harus di ubah menjadi double.
+2. Modifikasi kode program di atas dengan melakukan inisialisasi elemen array sekaligus 
+pada saat deklarasi array.
+package Jobsheet9;
+
+public class ArrayBilangan04 {
+    public static void main(String[] args) {
+        int [] bil={5,13,-7,17};
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
@@ -14,3 +28,13 @@ public class ArrayBilangan04 {
         System.out.println(bil[3]);
     }
 }
+3. Apa keluaran dari program? Jelaskan maksud dari statement tersebut.
+5
+13
+-7
+17
+menampilkan lopingan dari 0 sampai 4.
+4. Jika kondisi pada statement for-loop di atas diubah menjadi: i <= 4, apa keluaran 
+dari program? Mengapa demikian?
+maka akan terjadi ArrayIndexOutOfBoundsException karena mencoba mengakses elemen di luar batas array (bil[4] tidak ada).
+ */

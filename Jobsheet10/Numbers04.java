09c296dfe07f26e06cd76c11d89456e26630b549
alfatitahalifia-source <alfatitahalifia@gmail.com>
2025-11-20 18:28:17 +0700

    Percobaaan 3 Array2

---
+++ Jobsheet10/Numbers04.java
@@ -0,0 +1,10 @@
+package Jobsheet10;
+
+public class Numbers04 {
+    public static void main(String[] args) {
+        int [][] myNumber=new int[3][];
+        myNumber[0]=new int[5];
+        myNumber[1]=new int[3];
+        myNumber[2]=new int[1];
+    }
+}

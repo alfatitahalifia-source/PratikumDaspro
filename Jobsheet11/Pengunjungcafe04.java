1fbdde22e38dbd35fd1f1c68310753315ce8ff1d
alfatitahalifia-source <alfatitahalifia@gmail.com>
2025-12-02 21:23:00 +0700

    Percobaan 4 Fungsi 1

---
+++ Jobsheet11/Pengunjungcafe04.java
@@ -0,0 +1,15 @@
+package Jobsheet11;
+
+public class Pengunjungcafe04 {
+    public static void main(String[] args) {
+        daftarPengunjung("Ali","Budi","Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+    public static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung : ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- "+namaPengunjung[i]);
+        }
+    }
+}
